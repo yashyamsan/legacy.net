2b6c569cacb7ce72b32f37eac75f962c966e781b
Yash Yamsanwar <yashyam.yy@gmail.com>
2023-10-16 17:13:56 -0700

    build and deploy

--- aws/scripts/after-install.ps1
+++ aws/scripts/after-install.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy RemoteSigned -Force
 Import-Module AWSPowerShell
 cd C:\inetpub\wwwroot 
-Copy-S3Object -BucketName codedeploystack-webappdeploymentbucket-157n7dpyi3hbh -Key deployment.zip -LocalFile c:\inetpub\wwwroot\deployment.zip
+Copy-S3Object -BucketName codedeploy-1234 -Key deployment.zip -LocalFile c:\inetpub\wwwroot\deployment.zip
 Expand-Archive C:\inetpub\wwwroot\deployment.zip -DestinationPath C:\inetpub\wwwroot\
 rm .\deployment.zip
 start http://127.0.0.1
